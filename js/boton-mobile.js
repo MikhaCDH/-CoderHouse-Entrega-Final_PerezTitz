41788c378b4c2d40677cc1f0fbb29aa8bdda5419
MikhaCDH <niuhen8@gmail.com>
2024-07-06 16:10:42 -0300

    Bug Fixes

--- js/boton-mobile.js
+++ js/boton-mobile.js
@@ -3,14 +3,14 @@ document.addEventListener("DOMContentLoaded", function() {
     var searchBar = document.querySelector('.buscar');
 
     searchButton.addEventListener('click', function() {
-        if (window.innerWidth <= 810) {
+        if (window.innerWidth <= 990) {
             searchBar.classList.toggle('active');
         }
     });
 
     // Para que la barra de búsqueda se contraiga si se hace clic fuera de ella
     document.addEventListener('click', function(event) {
-        if (!searchBar.contains(event.target) && window.innerWidth <= 810) {
+        if (!searchBar.contains(event.target) && window.innerWidth <= 990) {
             searchBar.classList.remove('active');
         }
     });
